241ec04525987ff21b90a057c445224813d57544
Rohith-6741 <rohithreddyyeruva629@gmail.com>
2025-11-28 07:13:50 +0000

    my frist project

---
+++ lib/s3stack.ts
@@ -0,0 +1,20 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+
+export class s3stack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+    
+    const bucket = new s3.Bucket(this, 'mybucketbucket', {
+      versioned: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+    });
+
+    new cdk.CfnOutput(this, 'BucketName', {
+       value:bucket.bucketName,
+    })
+    
+  }
+}
